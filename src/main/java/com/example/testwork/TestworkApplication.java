7164ff77598fb6282cfffa2329e4dd64854b1f05
Sidorov Dmitry <Sidorovdmitri6@mail.ru>
2022-10-25 18:00:56 +0300

    Work

--- src/main/java/com/example/testwork/TestworkApplication.java
+++ src/main/java/com/example/testwork/TestworkApplication.java
@@ -2,6 +2,8 @@ package com.example.testwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class TestworkApplication {
@@ -9,5 +11,8 @@ public class TestworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestworkApplication.class, args);
     }
-
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
