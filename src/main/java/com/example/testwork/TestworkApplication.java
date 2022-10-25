2708522949daa727a62ae2ad49370569cb0043e5
Sidorov Dmitry <Sidorovdmitri6@mail.ru>
2022-10-25 17:29:11 +0300

    Initial commit

---
+++ src/main/java/com/example/testwork/TestworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestworkApplication.class, args);
+    }
+
+}
